cfb905fae906f614970efe042d7099ea78c6e48e
ELLAGARTO-PC\User <santiguz1999@gmail.com>
2023-04-20 14:13:58 -0300

    Commit Prueba TP1

---
+++ TP 01/ClasesBase/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Cliente
+    {
+        private string Cli_DNI;
+
+        public string Cli_DNI1
+        {
+            get { return Cli_DNI; }
+            set { Cli_DNI = value; }
+        }
+        private string Cli_Apellido;
+
+        public string Cli_Apellido1
+        {
+            get { return Cli_Apellido; }
+            set { Cli_Apellido = value; }
+        }
+        private string Cli_Nombre;
+
+        public string Cli_Nombre1
+        {
+            get { return Cli_Nombre; }
+            set { Cli_Nombre = value; }
+        }
+        private string Cli_Direccion;
+
+        public string Cli_Direccion1
+        {
+            get { return Cli_Direccion; }
+            set { Cli_Direccion = value; }
+        }
+        private string OS_CUIT;
+
+        public string OS_CUIT1
+        {
+            get { return OS_CUIT; }
+            set { OS_CUIT = value; }
+        }
+        private string Cli_NroCarnet;
+
+        public string Cli_NroCarnet1
+        {
+            get { return Cli_NroCarnet; }
+            set { Cli_NroCarnet = value; }
+        }
+    }
+}
---
+++ TP 01/ClasesBase/ObraSocial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class ObraSocial
+    {
+        private string OS_CUIT;
+
+        public string OS_CUIT1
+        {
+            get { return OS_CUIT; }
+            set { OS_CUIT = value; }
+        }
+        private string OS_RazonSocial;
+
+        public string OS_RazonSocial1
+        {
+            get { return OS_RazonSocial; }
+            set { OS_RazonSocial = value; }
+        }
+        private string OS_Direccion;
+
+        public string OS_Direccion1
+        {
+            get { return OS_Direccion; }
+            set { OS_Direccion = value; }
+        }
+        private string OS_Telefono;
+
+        public string OS_Telefono1
+        {
+            get { return OS_Telefono; }
+            set { OS_Telefono = value; }
+        }
+    }
+}
