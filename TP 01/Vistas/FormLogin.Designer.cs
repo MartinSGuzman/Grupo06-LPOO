6cec075308994472ec3a249b4c5592423ecc74d9
Tiago <tiago.brizuela@invoitrade.com>
2023-05-19 22:46:58 -0300

    Revert "Delete TP 01 directory"
    This reverts commit 2d2090a9ca4ca3bd957f6b0fecbf68852cdffb13.

---
+++ TP 01/Vistas/FormLogin.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Vistas
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.usrName = new System.Windows.Forms.TextBox();
+            this.passLogin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(105, 60);
+            this.Email.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(43, 13);
+            this.Email.TabIndex = 1;
+            this.Email.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(259, 118);
+            this.Login.Margin = new System.Windows.Forms.Padding(2);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(50, 22);
+            this.Login.TabIndex = 5;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // usrName
+            // 
+            this.usrName.Location = new System.Drawing.Point(197, 58);
+            this.usrName.Margin = new System.Windows.Forms.Padding(2);
+            this.usrName.Name = "usrName";
+            this.usrName.Size = new System.Drawing.Size(68, 20);
+            this.usrName.TabIndex = 6;
+            this.usrName.TextChanged += new System.EventHandler(this.usrName_TextChanged);
+            // 
+            // passLogin
+            // 
+            this.passLogin.Location = new System.Drawing.Point(197, 84);
+            this.passLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.passLogin.Name = "passLogin";
+            this.passLogin.Size = new System.Drawing.Size(68, 20);
+            this.passLogin.TabIndex = 7;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 159);
+            this.Controls.Add(this.passLogin);
+            this.Controls.Add(this.usrName);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormLogin";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.TextBox usrName;
+        private System.Windows.Forms.TextBox passLogin;
+    }
+}
